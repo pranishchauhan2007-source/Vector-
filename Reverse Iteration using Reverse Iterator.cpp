2a8dea7e9a18a8f5d675bea9b5b33a4374565e89
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-08-12 22:40:57 +0530

    Reverse iteration using reverse iterator

---
+++ Reverse Iteration using Reverse Iterator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    for(auto it = v.rbegin(); it != v.rend(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
