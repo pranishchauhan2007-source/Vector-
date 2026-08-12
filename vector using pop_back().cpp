60260446610e746fd7b62e4f46cddc6e31de45a7
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-08-12 22:41:59 +0530

    Vector using pop_ back()

---
+++ vector using pop_back().cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10, 20, 30, 40, 50};
+    v.pop_back();
+
+    for(auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
