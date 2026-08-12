0d0940feb2815e6d5145c206076c061b6a68dd98
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-08-12 22:46:36 +0530

    Vector using erase()

---
+++ vector using erase().cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    v.erase(v.begin() + 2);
+
+    for(auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
